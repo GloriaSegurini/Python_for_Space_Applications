94a5d849f24d55ce9b2abe4f17271a5ba038ec1b
Segurini <g.segurini@studenti.unipi.it>
2024-08-10 20:48:42 +0200

    new calculus

--- code/M5 - Earth's orbit around the sun/earth.py
+++ code/M5 - Earth's orbit around the sun/earth.py
@@ -55,3 +55,55 @@ v = np.empty(shape=(len(t),2))
 
 # Set initial conditions for position and velocity
 r[0], v[0] = r_0, v_0
+
+# Define a function that returns the acceleration vector of a given body when passed into the position vector
+r_size = np.linalg.norm(r)
+
+def accn(r):
+    return (-G*M / r_size**3)*r
+
+# Euler Integration
+def euler(r,v,accn,dt):
+    # objective: update empty arrays for r,v  with the simulated data
+    '''
+    ODE (ordinary differential equation) for Position
+    -> dr/dt = v
+    velocity is the rate of change at position r
+    -> r_new = r_old + v_old*dt
+
+    
+    ODE for Velocity
+    -> dv/dt = a
+    -> n_new = v_old + a(r_old)*dt
+    '''
+
+    for i in range(1, len(t)): #not 0, because we already have the initial vetors fo r and v
+        r[i] = r[i-1] + v[i-1]*dt
+        v[i] = v[i-1] + accn(r[i-1])*dt
+
+
+# Apply the Euler Integration on the given conditions 
+euler(r,v,accn,dt)
+
+# Find the point at which Earth is at its Aphelion
+sizes = [np.linalg.norm(position) for position in r]
+pos_aphelion = np.max(sizes)
+arg_aphelion = np.argmax(sizes)
+vel_aphelion = np.linalg.norm(v[arg_aphelion])
+print("pos_aphelion: ", pos_aphelion, "arg_aphelion: ",arg_aphelion, r[arg_aphelion])
+
+'''
+# Calcolo delle derivate numeriche dx/dt e dy/dt
+dx_dt = np.diff(r[:,0]) / dt  # Derivata numerica di x rispetto al tempo
+dy_dt = np.diff(r[:,1]) / dt  # Derivata numerica di y rispetto al tempo
+
+# Calcolo della velocità scalare usando la norma delle derivate
+vel_numerical = np.sqrt(dx_dt**2 + dy_dt**2)
+
+# Confronto tra la velocità scalare calcolata direttamente e quella calcolata con le derivate
+vel_aphelion_numerical = vel_numerical[arg_aphelion-1]  # Nota: np.diff riduce la dimensione di 1
+vel_aphelion_direct = np.linalg.norm(v[arg_aphelion])
+
+print(f"Velocità all'afelio usando la norma di (dx/dt, dy/dt): {vel_aphelion_numerical}")
+print(f"Velocità all'afelio calcolata direttamente: {vel_aphelion_direct}")
+'''
